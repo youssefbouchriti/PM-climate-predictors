505035bf6dbe26ebe1e1ce406549155b147db1e5
youssefbouchriti <youssef.bouchriti@edu.uiz.ac.ma>
2026-04-11 08:13:22 +0100

    Add files via upload

---
+++ Customization.r
@@ -0,0 +1,10 @@
+# Model parameters
+ntree = 500          # Number of trees
+mtry = 3             # Variables sampled at each split
+
+# Validation parameters
+cv_folds = 5         # Number of cross-validation folds
+train_ratio = 0.8    # Training/test split ratio
+
+# File paths
+main_dir <- "D:/00-research/my_papers/PM Climat Predictors/random forest model PM"
---
+++ Step2_run the script.r
@@ -0,0 +1,2 @@
+# Open R or RStudio
+source("random_forest_pm25_analysis.R")
